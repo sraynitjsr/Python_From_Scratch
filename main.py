5cdad83a4f21bff4a3820cc8768d06adef4cfee4
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-11 17:44:29 +0530

    Reading command line arguments using python

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import sys
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python program.py [arg1] [arg2] ...")
-        return
+    # Check if the correct number of command line arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <string> <integer> <float> <character>")
+        sys.exit(1)
 
-    script_name = sys.argv[0]
-    
-    arguments = sys.argv[1:]
-    
-    print("Script Name:", script_name)
+    # Reading command line arguments
+    input_string = sys.argv[1]
+    input_integer = int(sys.argv[2])
+    input_float = float(sys.argv[3])
+    input_character = sys.argv[4][0]
 
-    print("Arguments:", arguments)
+    # Printing the inputs
+    print("String:", input_string)
+    print("Integer:", input_integer)
+    print("Float:", input_float)
+    print("Character:", input_character)
 
 if __name__ == "__main__":
     main()
