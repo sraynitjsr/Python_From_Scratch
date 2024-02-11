fcf82dbe6586cc9a07b8c8e1b4a19d825f3b1bf4
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-11 17:21:29 +0530

    Command Line Arguments in Python

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python program.py [arg1] [arg2] ...")
+        return
+
+    script_name = sys.argv[0]
+    
+    arguments = sys.argv[1:]
+    
+    print("Script Name:", script_name)
+
+    print("Arguments:", arguments)
+
+if __name__ == "__main__":
+    main()
