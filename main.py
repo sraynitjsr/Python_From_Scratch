f777d9669bb23c65abb2bc181030506cfdfb1947
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-13 23:42:39 +0530

    Loops in python

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from data_types import data_types
+from loops import loops
 
 def main():
     print("Hello, World!")
 
 if __name__ == "__main__":
     main()
+
+    print("\n")
+    
     data_types()
+
+    print("\n")
+
+    loops()
