7e22778caec75fecb64c2d4654a9cd6ce42e0b0a
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-02-16 00:23:38 +0530

    Lists in Python

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from data_types import data_types
 from loops import loops
+from lists import lists
 
 def main():
     print("Hello, World!")
 
 if __name__ == "__main__":
     main()
-
     print("\n")
     
     data_types()
-
     print("\n")
 
     loops()
+    print("\n")
+
+    lists()
+    print("\n")
