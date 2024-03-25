64a645fed7cd7dd49fa7abcabf7d4468165ff8ca
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-03-25 23:17:34 +0530

    String Formatting Named Placeholder

--- string_formatting.py
+++ string_formatting.py
@@ -7,3 +7,7 @@ player2 = "Rohit Sharma"
 age2 = 37
 formatted_string_2 = "Player: {}, Age: {}".format(player2, age2)
 print(formatted_string_2)
+
+player_info = {'name': 'Jasprit Bumrah', 'age': 28}
+formatted_string_4 = "Player: {name}, Age: {age}".format(**player_info)
+print(formatted_string_4)
