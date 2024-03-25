a84b5cfc43ab8f7d64441194efd8aa4666a2a7f1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-03-25 23:04:51 +0530

    str.format() way of formatting string in python

--- string_formatting.py
+++ string_formatting.py
@@ -1,4 +1,9 @@
-player = "Virat Kohli"
-age = 35
-formatted_string = "Player: %s, Age: %d" % (player, age)
-print(formatted_string)
+player1 = "Virat Kohli"
+age1 = 35
+formatted_string_1 = "Player: %s, Age: %d" % (player1, age1)
+print(formatted_string_1)
+
+player2 = "Rohit Sharma"
+age2 = 37
+formatted_string_2 = "Player: {}, Age: {}".format(player2, age2)
+print(formatted_string_2)
