3afc5a63e6500d0ca0626b02c6cba5e4c5de212c
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-10 23:09:44 +0530

    Abstraction Using Python

---
+++ abstraction.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+rectangle = Rectangle(5, 4)
+print("Area of Rectangle:", rectangle.area())
